bcc75b999f8e3d2ff5954635c2a2c274a3780101
Fleynaro <dariusjohnson555@gmail.com>
2019-01-08 11:36:32 +0500

    Add files via upload

--- src/ColAndreas.h
+++ src/ColAndreas.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Bet this is it here
 
 #ifndef _INCL_COLANDREAS
@@ -34,43 +33,4 @@ extern bool colInit;
 extern bool colDataLoaded;
 extern cell nullAddress;
 extern logprintf_t				logprintf;
-
-=======
-// Bet this is it here
-
-#ifndef _INCL_COLANDREAS
-#define _INCL_COLANDREAS
-
-#pragma warning (disable:4005 700 996)
-#if (defined(WIN32) || defined(_WIN32) || defined(_WIN64))
-#include <windows.h>
-#define EXPORT __declspec(dllexport)
-#else
-#include "pthread.h"
-#endif
-#include "sstream"
-#if defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD__) || defined(__OpenBSD__)
-#include "stdarg.h"
-#define EXPORT __attribute__((visibility("default")))
-#endif
-
-#ifndef WIN32
-typedef unsigned long DWORD;
-#define MAX_PATH 250
-#endif
-
-// AMX
-typedef void(*logprintf_t)(char* format, ...);
-#include "SDK/amx/amx.h"
-#include "SDK/plugincommon.h"
-
-// Standard includes
-#include <vector>
-
-extern bool colInit;
-extern bool colDataLoaded;
-extern cell nullAddress;
-extern logprintf_t				logprintf;
-
->>>>>>> d7537e9c90cf6bd17408e3a71e7f8db631f3cd78
 #endif
